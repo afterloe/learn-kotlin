00a9294e73b4e98904c33e263ac7fee08677fbb9
afterloe <lm6289511@gmail.com>
2018-07-17 14:11:26 +0800

    feat: 协程的取消的几种方式、超时限制、强制执行三种操作

--- src/main/kotlin/cn/awpaas/learn/day05/Learn-3.kt
+++ src/main/kotlin/cn/awpaas/learn/day05/Learn-3.kt
@@ -1,8 +1,6 @@
 package cn.awpaas.learn.day05
 
-import kotlinx.coroutines.experimental.delay
-import kotlinx.coroutines.experimental.launch
-import kotlinx.coroutines.experimental.runBlocking
+import kotlinx.coroutines.experimental.*
 
 /*
 等待协程
@@ -25,8 +23,35 @@ suspend fun job2() {
 }
 
 fun waitToComplete() = runBlocking {
-    val son1 = launch { job() }
-    val son2 = launch { job2() }
+    val son1 = launch {
+        job()
+        // 如果suspend 函数中正在进行循环操作，调用cancel 取消协程，isActive虽然为false， 但是循环中的代码是继续运行的
+
+        // 处理方式一:
+        /*
+         while(true) {
+            if (!isActive) {
+                return@launch
+            }
+         }
+         */
+//        if (!isActive) return@launch
+
+        // 处理方式二：
+        // 直接调度yield 函数，他是捕获 isCompleted, 如果 isCompleted 返回true，循环的时候自动抛出异常 达到终止循环的操作
+//        yield()
+    }
+    val son2 = launch {
+        job2()
+        // run 与 NonCancellable 结合，这里面的代码块是一定不会被取消的
+        run(NonCancellable) {
+            println("123 -> ")
+        }
+        // 设置协程超时时间，如果超过时间 抛出异常终止
+        withTimeout(3000L) {
+            println("toDo")
+        }
+    }
     println("hi!")
     println("son1 is active? -> ${son1.isActive}; complete? -> ${son1.isCompleted}")
     println("son2 is active? -> ${son2.isActive}; complete? -> ${son2.isCompleted}")
@@ -34,6 +59,7 @@ fun waitToComplete() = runBlocking {
     println("son1 is active? -> ${son1.isActive}; complete? -> ${son1.isCompleted}")
     println("son2 is active? -> ${son2.isActive}; complete? -> ${son2.isCompleted}")
     son2.join()
+//    son1.cancel() // 取消协程 isActive 为true 并且 isCompleted 为false 的时候取消协程 返回是否成功的flag
     println("son1 is active? -> ${son1.isActive}; complete? -> ${son1.isCompleted}")
     println("son2 is active? -> ${son2.isActive}; complete? -> ${son2.isCompleted}")
 }
